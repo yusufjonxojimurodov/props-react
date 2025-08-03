5db667fc907533c381d497a8fe526487441e8d18
leviacerman123 <dapergnat@gmail.com>
2025-08-03 14:16:44 +0500

    aziz

--- src/components/Jasur.jsx
+++ src/components/Jasur.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Jasur = () => {
   return (
-    <div>Jasur</div>
+    <div></div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "../src/assets/style/index.css"
+import "./assets/style/index.css"
 import Jasur from '../src/components/Jasur'
 import Aziz from '../src/components/Aziz'
 createRoot(document.getElementById('root')).render(
